6170fab0450569c9087fee3051fc37b82728c9b2
LJ <jun.lu.726@gmail.com>
2014-06-17 09:47:35 +0800

    BAIDU TONGJI

--- app/v/ad/footer.php
+++ app/v/ad/footer.php
@@ -5,4 +5,9 @@
 </div>
 
 </body>
+<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F6355ea34f1baf8dd115bdfa3c99e7929' type='text/javascript'%3E%3C/script%3E"));
+</script>
+
 </html>
--- app/v/front/footer.php
+++ app/v/front/footer.php
@@ -1,3 +1,9 @@
 <div id="footer"></div>
 </body>
+
+<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F6355ea34f1baf8dd115bdfa3c99e7929' type='text/javascript'%3E%3C/script%3E"));
+</script>
+
 </html>
